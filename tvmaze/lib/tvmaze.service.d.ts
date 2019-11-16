cd22abdd4db622b8c92ea40a5d83d102e68df448
Ravish Bhat <ravish1479@gmail.com>
2019-11-16 15:02:18 +0530

    Auto-generated commit

--- tvmaze/lib/tvmaze.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs';
-import { TvObject } from './tvmaze.models';
-import { HttpClient } from '@angular/common/http';
-export declare class TvmazeService {
-    private http;
-    private readonly apiRoot;
-    constructor(http: HttpClient);
-    getShow(id: number): Observable<TvObject>;
-}
--- tvmaze/public-api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/tvmaze.service';
-export * from './lib/tvmaze.component';
-export * from './lib/tvmaze.module';
-export * from './lib/tvmaze.models';
